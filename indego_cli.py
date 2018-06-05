6c95e96be8281829238bd5b8e0f84934a3feb13b
Eric OC <eric@ericoc.com>
2018-06-05 19:02:38 -0400

    working cli script

---
+++ indego_cli.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+from indego import Indego
+import sys
+from pprint import pprint
+
+# Instantiate the Indego class
+indego = Indego();
+
+# Get the stations that were requested if doing a search with a CLI argument
+if len(sys.argv) > 1:
+	search = sys.argv[1]
+
+# Otherwise, get all stations
+else:
+	search = ''
+
+# Get stations
+stations = indego.get_stations(search)
+
+# Return red error message to standard error (stderr) if no stations were found and exit with non-zero
+if not stations:
+	sys.stderr.write("\x1b[31mNo stations found!\x1b[0m\n")
+	exit(1)
+
+# Loop through each bike-share station
+for station in stations:
+
+	# List the current stations ID and name, padded with spaces so everything lines up
+	print (str(stations[station]['kioskId']).ljust(8) + ' ' + stations[station]['name'].ljust(60), end='')
+
+	# Build a graph for bikes at the current station
+	graph = ''
+	for bike in range(stations[station]['bikesAvailable']):
+		graph += '#'
+
+	# And continue building the graph for empty docks at the current station
+	for dock in range(stations[station]['docksAvailable']):
+		graph += '='
+
+	# Pad the graph with spaces to line stuff up and color code the bikes (#) vs. docks (=) graphs that we just built
+	graph = graph.ljust(42)
+	graph = graph.replace('#', '\x1b[32m#\x1b[0m')	# Bikes are green
+	graph = graph.replace('=', '\x1b[31m=\x1b[0m')	# Docks are red
+	print(graph, end='')
+
+	# Pad the bikes and docks numbers with spaces to line stuff up
+	bike_info = str(stations[station]['bikesAvailable']) + ' bikes'
+	dock_info = str(stations[station]['docksAvailable']) + ' docks'
+	print(bike_info.ljust(12), end='')
+	print(dock_info.ljust(12))
