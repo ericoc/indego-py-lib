1bd2dc6c846e881674fb51f722b04c9763eeea00
Eric OCallaghan <eric.ocallaghan@jefferson.edu>
2019-05-08 13:36:32 -0400

    bump vers. no

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="indego",
-    version="1.0.4",
+    version="1.0.5",
     author="Eric O'Callaghan",
     author_email="eric@ericoc.com",
     description="Python3 library for working with the Philadelphia Indego Bike Share API",
