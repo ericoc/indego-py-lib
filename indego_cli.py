e1bdafc924ce1ae4d69ac26a4401f1ac5f9f4a37
Eric OC <eric@ericoc.com>
2019-05-31 19:23:23 -0400

    this is just cleaner

--- indego_cli.py
+++ indego_cli.py
@@ -23,18 +23,18 @@ if not stations:
     exit(1)
 
 # Loop through each bike-share station
-for station in stations:
+for station_id, station in stations.items():
 
     # List the current stations ID and name, padded with spaces so everything lines up
-    print (str(stations[station]['kioskId']).ljust(8) + ' ' + stations[station]['name'].ljust(60), end='')
+    print (str(station_id).ljust(8) + ' ' + station['name'].ljust(60), end='')
 
     # Build a graph for bikes at the current station
     graph = ''
-    for bike in range(stations[station]['bikesAvailable']):
+    for bike in range(station['bikesAvailable']):
         graph += '#'
 
     # And continue building the graph for empty docks at the current station
-    for dock in range(stations[station]['docksAvailable']):
+    for dock in range(station['docksAvailable']):
         graph += '='
 
     # Pad the graph with spaces to line stuff up and color code the bikes (#) vs. docks (=) graphs that we just built
@@ -44,7 +44,7 @@ for station in stations:
     print(graph, end='')
 
     # Pad the bikes and docks numbers with spaces to line stuff up
-    bike_info = str(stations[station]['bikesAvailable']) + ' bikes'
-    dock_info = str(stations[station]['docksAvailable']) + ' docks'
+    bike_info = str(station['bikesAvailable']) + ' bikes'
+    dock_info = str(station['docksAvailable']) + ' docks'
     print(bike_info.ljust(12), end='')
     print(dock_info.ljust(12))
