b604009b9260429e2fa8dff7cbeac3db8ba8b8f1
Eric O'Callaghan <eric@ericoc.com>
2025-05-11 15:52:39 -0400

    update broken URL, university example, and version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="indego",
-    version="1.1.0",
+    version="1.1.1",
     author="Eric O'Callaghan",
     author_email="eric@ericoc.com",
     description="Python3 library for working with the Philadelphia Indego Bike Share API",
